a3572788d239f60ae421bf44bc4c777f7950e2ef
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-10 22:28:27 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+//Программа который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число А в натуральной степене В =  " + Math.Pow(numberA, numberB));
+*/
