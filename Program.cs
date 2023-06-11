5fb393d8208babf80afcaf677c54b487dec0fde4
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-11 15:50:22 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -7,3 +7,33 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Число А в натуральной степене В =  " + Math.Pow(numberA, numberB));
 */
+
+
+//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите  число: ");
+var Number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int i = 0;
+
+for(i = 0; i < Number; i++)
+{
+sum = sum + Number % 10;
+Number = Number /10;
+}
+
+  Console.WriteLine(sum );
+
+
+
+/*
+//Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+int size = 8;
+int[] array = new int[size]; 
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    //Console.Write( + array[i] + " ");                                                              ]");
+}
+Console.Write($"Массив: [ {String.Join("; ", array)} ]");
+*/
